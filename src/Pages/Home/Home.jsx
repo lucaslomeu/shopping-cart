f6611e50d5a1d1b3708aa0c3cc532601ae76f2aa
lucaslomeu <lucaslomeugomes@gmail.com>
2022-02-04 19:04:54 -0300

    Update website

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Header from '../../components/Header/Header';
+import Products from '../../components/Products/Products';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
