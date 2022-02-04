5703466c31dbb7bcae4cb917256d1c50a372b957
lucaslomeu <lucaslomeugomes@gmail.com>
2022-02-04 19:03:18 -0300

    Added fake api

---
+++ src/storage.js
@@ -0,0 +1,53 @@
+import Item1 from './assets/images/item1.jpg';
+import Item2 from './assets/images/item2.jpg';
+import Item3 from './assets/images/item3.jpg';
+import Item4 from './assets/images/item4.jpg';
+
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item1,
+      availableColors: '2',
+    },
+    {
+      id: '2',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item2,
+      availableColors: '2',
+    },
+    {
+      id: '3',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item3,
+      availableColors: '2',
+    },
+    {
+      id: '3',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item4,
+      availableColors: '2',
+    },
+    {
+      id: '1',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item1,
+      availableColors: '2',
+    },
+    {
+      id: '2',
+      name: 'T-Shirt',
+      price: 120,
+      image: Item2,
+      availableColors: '2',
+    },
+  ],
+};
+
+export default data;
